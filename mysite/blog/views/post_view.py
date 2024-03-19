c7011238a5df117953a066eb42e9bec133e2a7b2
Fernando Scherer <fernando.scherer96@hotmail.com>
2024-03-18 21:41:47 -0300

    Atualizando a branch de templaites

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -1,22 +1,19 @@
 # Importa a classe HttpResponse do módulo django.http.
 # HttpResponse é usada para enviar respostas HTTP do servidor para o cliente.
-from django.http import HttpResponse
 
 # Importa a classe genérica View de django.views.
 # A classe genérica View fornece uma maneira de definir views baseadas em classes.
 from django.views import generic
 
+from blog.models import Post
+
 # Define a classe PostView, que herda de generic.View.
 # Isso permite que você crie uma view personalizada com métodos para lidar
 # com diferentes tipos de requisições HTTP (como GET, POST, etc.).
-class PostView(generic.View):
+class PostView(generic.ListView):
+    queryset = Post.objects.filter(status = 1).order_by('-created_on')
+    template_name = 'index.html'
 
-    # Define o método get, que é chamado quando uma requisição HTTP GET é recebida.
-    # 'self' refere-se à instância da classe PostView.
-    # 'request' é o objeto HttpRequest recebido, contendo informações sobre a requisição HTTP.
-    # '*args' e '**kwargs' são argumentos e argumentos de palavra-chave variáveis que podem ser passados,
-    # mas não são usados neste método específico.
-    def get(self, request, *args, **kwargs):
-        # Retorna um objeto HttpResponse contendo o texto 'Hello World'.
-        # Isso será exibido ao cliente que fez a requisição GET.
-        return HttpResponse('Hello World')
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
